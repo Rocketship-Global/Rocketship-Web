997aa51054bc64b3740ca73d9876c90112e1d7ad
Leon Sandøy <leon.sandoy@conmodo.com>
2019-01-06 21:19:15 +0100

    Login is mostly working.

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import View
 
 
-def home(request):
-    return render(request, "main/bulma.html")
+class Home(LoginRequiredMixin, View):
+    login_url = "/accounts/login/"
+
+    def get(self, request):
+        return render(request, "main/home.html")
